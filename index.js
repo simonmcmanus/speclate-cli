c119f7501049310cee5d74fb8ef10f3d10087c2e
Simon McManus <mcmanus.simon@gmail.com>
2016-09-11 11:09:01 +0100

    linting fixes

--- index.js
+++ index.js
@@ -3,69 +3,69 @@ var generateApi = require('./lib/generate-api')
 var moveFiles = require('./lib/move-files')
 var schema = require('./lib/schema')
 var async = require('async')
-var package = require(__dirname + '/package.json');
-var program = require('commander');
+var path = require('path')
+var packagePath = path.join(__dirname, '/package.json')
+var pkg = require(packagePath)
+var program = require('commander')
 
-module.exports = function(spec, speclateVersion) {
+module.exports = function (spec, speclateVersion) {
+  console.log('Speclate v' + speclateVersion, 'cli v' + pkg.version)
 
-    console.log('Speclate v' + speclateVersion, 'cli v' + package.version)
-
-    program
+  program
     .version('0.0.1')
     .option('-A, --all', 'run all commands')
     .option('-S, --specs', 'generate spec api files')
     .option('-F, --files', 'Move files')
     .option('-V, --validate', 'validate schema')
     .option('-M, --markup', 'generate markup')
-    .parse(process.argv);
+    .parse(process.argv)
 
-    var methods = {
-        validate: (next) => {
-            console.log('')
-            console.log('Validating spec..');
-            schema.validate(spec);
-            console.log('  ok')
-            next();
-        },
-        markup: (next) => {
-            console.log('')
-            console.log('Generating markup..');
-            generateMarkup(spec, next);
-        },
-        api: (next) => {
-            console.log('')
-            console.log('Generating API..');
-            generateApi(spec, next);
+  var methods = {
+    validate: (next) => {
+      console.log('')
+      console.log('Validating spec..')
+      schema.validate(spec)
+      console.log('  ok')
+      next()
+    },
+    markup: (next) => {
+      console.log('')
+      console.log('Generating markup..')
+      generateMarkup(spec, next)
+    },
+    api: (next) => {
+      console.log('')
+      console.log('Generating API..')
+      generateApi(spec, next)
+    },
+    files: (next) => {
+      console.log('')
+      console.log('Moving files..')
+      moveFiles(spec, next)
+    }
+  }
 
-        },
-        files: (next) => {
-            console.log('')
-            console.log('Moving files..')
-            moveFiles(spec, next);
-        }
+  if (program.all) {
+    async.series(methods)
+  } else {
+    var run = []
+
+    if (program.validate) {
+      schema.validate(spec)
     }
 
-    if(program.all) {
-        async.series(methods);
-    } else {
-        var run = [];
-
-        if (program.validate) {
-            schema.validate(spec);
-        }
-
-        if (program.markup) {
-            run.push(methods.markup);
-        }
-
-        if (program.api) {
-            run.push(methods.api);
-        }
-
-        if (program.files) {
-            run.push(methods.files);
-        }
-
-        async.series(run);
+    if (program.markup) {
+      run.push(methods.markup)
     }
+
+    if (program.api) {
+      run.push(methods.api)
+    }
+
+    if (program.files) {
+      run.push(methods.files)
+    }
+
+    async.series(run)
+  }
 }
