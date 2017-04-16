1285768a0f065805d2f1c6b2fd5c5643f9dd7727
Simon McManus <mcmanus.simon@gmail.com>
2017-04-16 09:59:07 +0100

    dynamic routes working with tests

---
+++ spec/dynamic-routes-spec.js
@@ -0,0 +1,66 @@
+var dynamicRoutes = require('../lib/dynamic-routes')
+
+describe('given a spec with dynamic routes ', function () {
+
+  var spec = {
+    '/:date/index.html': {
+      page: 'link',
+      data: [
+        {
+          '.created': '2007-12-12',
+          title: 'item 1'
+        },
+        {
+          '.created': '2007-10-10',
+          title: 'item 2'
+        }
+      ],
+      group: function (pages, item) {
+        var created = item['.created']
+        if (!pages[created]) {
+          pages[created] = []
+        }
+        //item.group = created
+        pages[created].push(item)
+        return pages
+      },
+      spec: {
+        title: 'page title',
+        '.links_holder': {
+          component: 'link'
+        }
+      }
+    }
+  }
+  describe ('calling dynmaic-routes', function () {
+
+    var generatedRoutes = dynamicRoutes(spec);
+    //console.log(generatedRoutes)
+
+    it('should not change the title value', () => {
+      expect(generatedRoutes['/links/2007-10-10/index.html'].spec.title).toEqual('page title')
+    });
+
+    it('Should generate the dynamic route for 2007-10-10', () => {
+
+      expect(generatedRoutes['/links/2007-10-10/index.html'].spec['.links_holder'].data.length).toEqual(1)
+      expect(generatedRoutes['/links/2007-10-10/index.html'].spec['.links_holder'].data[0]['.created']).toEqual('2007-10-10')
+    })
+
+
+
+
+    it('should not change the title value', () => {
+      expect(generatedRoutes['/links/2007-12-12/index.html'].spec.title).toEqual('page title')
+    });
+
+    it('Should generate the dynamic route for 2007-12-12', () => {
+      expect(generatedRoutes['/links/2007-12-12/index.html'].spec['.links_holder'].data.length).toEqual(1)
+      expect(generatedRoutes['/links/2007-12-12/index.html'].spec['.links_holder'].data[0]['.created']).toEqual('2007-12-12')
+    })
+
+    it('should remove the orignal dynamic route', () => {
+      expect(generatedRoutes['/:date/index.html']).toEqual(undefined)
+    })
+  })
+})
